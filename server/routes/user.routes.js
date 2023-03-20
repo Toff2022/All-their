cfc9e310d86cf62109d190ea737b6b4ad804f3ed
Anton Peshekhonov <noviyya2021@gmail.com>
2023-03-20 21:21:11 +0300

    add auth.middleware

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,18 +1,38 @@
 const express = require("express")
 const router = express.Router({ mergeParams: true })
 const User = require("../models/User")
+const auth = require("../middleware/auth.middleware")
 const chalk = require("chalk")
+
+router.patch("/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params
+        // todo: userId === current userId
+        if (userId) {
+            const updateUser = await User.findByIdAndUpdate(userId, req.body, { new: true })
+            res.send(updateUser)
+        } else {
+            res.status(401).json({ message: "Unautorized!" })
+        }
+
+    } catch (e) {
+        res.status(500).json({
+            messge: "На сервере userId произошла ошибка. Попробуйте позже!"
+        })
+    }
+})
 
 router.get("/", async (req, res) => {
     try {
         const user = await User.find()
-        res.send(user)
-        // console.log(chalk.grey("user", user));
+        res.status(200).send(user)
+
     } catch (e) {
         res.status(500).json({
-            messge: "На сервере произошла ошибка при запросе User. Попробуйте позже!"
+            messge: "На сервере user произошла ошибка. Попробуйте позже!"
         })
     }
+
 })
 
 module.exports = router
